980330252bd71fb1a88c6374ba2c0447fb9534e3
Colin <colin.combe@ed.ac.uk>
2015-06-23 21:54:41 +0100

    legend

--- network.php
+++ network.php
@@ -25,9 +25,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="common platform for downstream analysis of CLMS data" />
-<!--	//do we need following?
-		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
--->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<link rel="icon" type="image/ico" href="./images/favicon.ico">
@@ -48,13 +46,11 @@
 -->
       
         <!--spectrum dev-->
-<!--
         <script type="text/javascript" src="../spectrum/src/SpectrumViewer.js"></script>
         <script type="text/javascript" src="../spectrum/src/PeptideFragmentationKey.js"></script>
         <script type="text/javascript" src="../spectrum/src/graph/Graph.js"></script>
         <script type="text/javascript" src="../spectrum/src/graph/Peak.js"></script>
         <script type="text/javascript" src="../spectrum/src/graph/PeakAnnotation.js"></script>
--->
         
         <!--xiNET dev-->
         <script type="text/javascript" src="../crosslink-viewer/src/controller/Init.js"></script>
@@ -75,6 +71,9 @@
         <script type="text/javascript" src="../crosslink-viewer/src/controller/ReadCSV.js"></script>
     </head>
     <body>	
+<!--
+    wrapper for the jquery ui dialogs
+-->
 		<div id="wrapper">
 <!--
 			<div id='spectrum'>
@@ -88,11 +87,58 @@
 			
 			<div class="" id="legendPanel">
 				<div id="colours"><img id="legend" src="./images/fig3_1.svg"><br><img id="logo" src="./images/logos/rappsilber-lab-small.png"></div>
-<!--
-			<div id="colours"></div>
--->
 			</div>	
+
+<!--
+			<div class="" id="helpPanel">
+				<table class="overlay-table"  bordercolor="#eee" >
+					<tr>
+						<td>Toggle the proteins between a bar and a circle</td>
+						<td>Click on protein</td>
+					</tr>
+					<tr>
+						<td>Zoom</td>
+						<td>Mouse wheel</td>
+					</tr>
+					<tr>
+						<td>Pan</td>
+						<td>Click and drag on background</td>
+					</tr>
+					<tr>
+						<td>Move protein</td>
+						<td>Click and drag on protein</td>
+					</tr>
+					<tr>
+						<td>Expand bar <br>(increases bar length until sequence is visible)</td>
+						<td>Shift_left-click on protein</td>
+					</tr>
+					<tr>
+						<td>Rotate bar</td>
+						<td>Click and drag on handles that appear at end of bar</td>
+					</tr>
+					<tr>
+						<td>Hide/show protein (and all links to it)</td>
+						<td>Right-click on protein</td>
+					</tr>
+					<tr>
+						<td>Hide links between two specific proteins</td>
+						<td>Right click on any link between those proteins</td>
+					</tr>
+					<tr>
+						<td>Show all hidden links</td>
+						<td>Right click on background</td>
+					</tr>
+					<tr>
+						<td>'Flip' self-links</td>
+						<td>Right-click on self-link</td>
+					</tr>
+				</table> 
+			</div>	
+-->
+
 		</div>	
+
+		
 		<!-- Main -->
 		<div id="main">
 
@@ -132,52 +178,7 @@
 				<div id="bottomDiv" class="overlay-box">
 						<p>No selection.</p>
 				</div>
-				<script>
-				//<![CDATA[		
-					var marginBottom = 95;
-					var minBottomDivHeight = 40;
-					var splitterDivHeight = 20;
-					var splitterDragging = false;
-					var splitterDiv = document.getElementById("splitterDiv");
-					var topDiv = document.getElementById("topDiv");
-					var bottomDiv = document.getElementById("bottomDiv");
-					var main = document.getElementById("main");
-					splitterDiv.onmousedown = function(evt) {
-						splitterDragging = true;
-					}
-					main.onmousemove = function(evt) {
-						if (splitterDragging === true || !evt){
-							var element = topDiv;
-							var top = 0;
-							do {
-								top += element.offsetTop  || 0;
-								element = element.offsetParent;
-							} while(element);
-							var topDivHeight;
-							if (evt) topDivHeight = evt.pageY - top - (splitterDivHeight / 2);
-							else topDivHeight = window.innerHeight - top - splitterDivHeight - minBottomDivHeight- marginBottom;
-							if (topDivHeight < 0) topDivHeight = 0;
-							var bottomDivHeight = window.innerHeight - top - topDivHeight - splitterDivHeight - marginBottom;
-							if (bottomDivHeight < minBottomDivHeight){
-								bottomDivHeight = minBottomDivHeight;
-								topDivHeight = window.innerHeight - top - splitterDivHeight - minBottomDivHeight- marginBottom;
-							}
-							topDiv.setAttribute("style", "height:"+topDivHeight+"px;");
-							bottomDiv.setAttribute("style", "height:"+bottomDivHeight+"px;");
-						};
-					}
-					main.onmouseup = function(evt) {
-						splitterDragging = false;
-					}
-					
-					main.onmousemove();
-					
-					window.onresize = function(event) {
-						main.onmousemove();//event);
-					};
-					
-				//]]>				  
-				</script>
+
 			</div>
 			
 			
@@ -247,132 +248,100 @@
 						</label>	
 					</div>		
 				</div>
-				<script type="text/javascript">	
-					//<![CDATA[					
-					var sliderDecimalPlaces = 1;
-					function getMinScore(){
-						if (xlv.scores){
-							var powerOfTen = Math.pow(10, sliderDecimalPlaces); 
-							return (Math.floor(xlv.scores.min * powerOfTen) / powerOfTen)
-									.toFixed(sliderDecimalPlaces);
-						}
-					}
-					function getMaxScore(){
-						if (xlv.scores){
-							var powerOfTen = Math.pow(10, sliderDecimalPlaces); 
-							return (Math.ceil(xlv.scores.max * powerOfTen) / powerOfTen)
-									.toFixed(sliderDecimalPlaces);
-						}
-					}
-					function sliderChanged(){
-						var slide = document.getElementById('slide');
-						var powerOfTen = Math.pow(10, sliderDecimalPlaces); 
-						
-						var cut = ((slide.value / 100) 
-									* (getMaxScore() - getMinScore()))
-									+ (getMinScore() / 1);
-						cut = cut.toFixed(sliderDecimalPlaces);
-						var cutoffLabel = document.getElementById("cutoffLabel");
-						cutoffLabel.innerHTML = '(' + cut + ')';
-						xlv.setCutOff(cut);
-					}						
-					//]]>
-				</script>	
 			</div>					
 		</div><!-- MAIN -->
-	
-		<!-- Slidey panels -->	
-		<div class="overlay-box" id="helpPanel">
-			<table class="overlay-table"  bordercolor="#eee" >
-				<tr>
-					<td>Toggle the proteins between a bar and a circle</td>
-					<td>Click on protein</td>
-				</tr>
-				<tr>
-					<td>Zoom</td>
-					<td>Mouse wheel</td>
-				</tr>
-				<tr>
-					<td>Pan</td>
-					<td>Click and drag on background</td>
-				</tr>
-				<tr>
-					<td>Move protein</td>
-					<td>Click and drag on protein</td>
-				</tr>
-				<tr>
-					<td>Expand bar <br>(increases bar length until sequence is visible)</td>
-					<td>Shift_left-click on protein</td>
-				</tr>
-				<tr>
-					<td>Rotate bar</td>
-					<td>Click and drag on handles that appear at end of bar</td>
-				</tr>
-				<tr>
-					<td>Hide/show protein (and all links to it)</td>
-					<td>Right-click on protein</td>
-				</tr>
-				<tr>
-					<td>Hide links between two specific proteins</td>
-					<td>Right click on any link between those proteins</td>
-				</tr>
-				<tr>
-					<td>Show all hidden links</td>
-					<td>Right click on background</td>
-				</tr>
-				<tr>
-					<td>'Flip' self-links</td>
-					<td>Right-click on self-link</td>
-				</tr>
-			</table> 
-		</div>	
+		
+<!--
+		horizontal splitter JS
+-->
+				<script>
+				//<![CDATA[		
+					var marginBottom = 95;
+					var minBottomDivHeight = 40;
+					var splitterDivHeight = 20;
+					var splitterDragging = false;
+					var splitterDiv = document.getElementById("splitterDiv");
+					var topDiv = document.getElementById("topDiv");
+					var bottomDiv = document.getElementById("bottomDiv");
+					var main = document.getElementById("main");
+					splitterDiv.onmousedown = function(evt) {
+						splitterDragging = true;
+					}
+					main.onmousemove = function(evt) {
+						if (splitterDragging === true || !evt){
+							var element = topDiv;
+							var top = 0;
+							do {
+								top += element.offsetTop  || 0;
+								element = element.offsetParent;
+							} while(element);
+							var topDivHeight;
+							if (evt) topDivHeight = evt.pageY - top - (splitterDivHeight / 2);
+							else topDivHeight = window.innerHeight - top - splitterDivHeight - minBottomDivHeight- marginBottom;
+							if (topDivHeight < 0) topDivHeight = 0;
+							var bottomDivHeight = window.innerHeight - top - topDivHeight - splitterDivHeight - marginBottom;
+							if (bottomDivHeight < minBottomDivHeight){
+								bottomDivHeight = minBottomDivHeight;
+								topDivHeight = window.innerHeight - top - splitterDivHeight - minBottomDivHeight- marginBottom;
+							}
+							topDiv.setAttribute("style", "height:"+topDivHeight+"px;");
+							bottomDiv.setAttribute("style", "height:"+bottomDivHeight+"px;");
+						};
+					}
+					main.onmouseup = function(evt) {
+						splitterDragging = false;
+					}
+					
+					main.onmousemove();
+					
+					window.onresize = function(event) {
+						main.onmousemove();//event);
+					};
+					
+				//]]>				  
+				</script>	
+				
+		<!-- floaty panels JS -->	
 
 		<script type="text/javascript">
 			//<![CDATA[
-			helpShown = false;
-			//~ legendShown = false;
-			function toggleHelpPanel() {
-				if (helpShown){
-					hideHelpPanel();
-				}
-				else {
-					showHelpPanel();
-				}
-			}
-			
-			//~ function toggleLegendPanel() {
-				//~ if (legendShown){
-					//~ hideLegendPanel();
-				//~ }
-				//~ else {
-					//~ showLegendPanel();
-				//~ }
-			//~ }
-			
-			function showHelpPanel() {
-					helpShown = true;
-					d3.select("#helpPanel").transition().style("height", "550px").style("top", "100px").duration(700);
-			}
-			function hideHelpPanel() {
-					helpShown = false;
-					d3.select("#helpPanel").transition().style("height", "0px").style("top", "-95px").duration(700);
-			}
-			//~ function showLegendPanel() {
-					//~ legendShown = true;
-					//~ d3.select("#legendPanel").transition().style("height", "550px").style("top", "100px").duration(700);
-			//~ }
-			//~ function hideLegendPanel() {
-					//~ legendShown = false;
-					//~ d3.select("#legendPanel").transition().style("height", "0px").style("top", "-95px").duration(700);
-//~ 
-			//~ }
-			
-			$("#legendPanel").dialog({resizable:true, autoOpen: false, width: 600, height: 450});
+			//forced to use jquery dialog for floaty internal frame
+	  	
+			$("#legendPanel").dialog({resizable:true, autoOpen: false, 
+								width: 360, height: "auto", position: { my: "left top", at: "left+40 top+100"}});
 			$('#legendPanel').on('dialogclose', function(event) {
 				document.getElementById('legendChkbx').checked = false;
 			});
+			
+			//~ $("#spectrum").dialog({resizable:true, autoOpen: false, width: 600, height: 450});
+			//~ //init spectrum viewer
+			//~ var pepFragDiv = document.getElementById('pepFragDiv');
+			//~ var graphDiv = document.getElementById('graphDiv');
+			//~ spectrumViewer = new SpectrumViewer(pepFragDiv, graphDiv);
+			//~ 
+			//~ function loadSpectra(id, pepSeq1, linkPos1, pepSeq2, linkPos2){
+				//~ //jquery dialog open
+				//~ $( "#spectrum" ).dialog("open");
+				//~ spectrumViewer.clear();
+				//~ var xmlhttp = new XMLHttpRequest();
+				//~ var url = "./php/spectra.php";
+				//~ var params =  "id=" + id;
+				//~ xmlhttp.open("POST", url, true);
+				//~ //Send the proper header information along with the request
+				//~ xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+				//~ xmlhttp.onreadystatechange = function() {//Call a function when the state changes.
+					//~ if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+						//~ spectrumViewer.setData(pepSeq1, linkPos1, pepSeq2, linkPos2, xmlhttp.responseText); 
+					//~ }
+				//~ }
+				//~ xmlhttp.send(params);
+			//~ };
 			//]]>
 		</script>
+
+<!--
+		xiNET init JS
+-->
 		
 		<script>
      		//<![CDATA[
@@ -438,10 +407,20 @@
 						table += "<tr style='height:10px;'></tr>";
 						for (var i = 0; i < domain.length; i ++){
 							//make transparent version of colour
+							//~ http://stackoverflow.com/questions/2049230/convert-rgba-color-to-rgb
 							var temp = new RGBColor(range[i%20]);
-							var trans = "rgba(" +temp.r+","+temp.g+","+temp.b+ ", 0.6)"
+							//~ Target.R = ((1 - Source.A) * BGColor.R) + (Source.A * Source.R)
+							//~ Target.G = ((1 - Source.A) * BGColor.G) + (Source.A * Source.G)
+							//~ Target.B = ((1 - Source.A) * BGColor.B) + (Source.A * Source.B)
+							var opaque = {};
+							opaque.r = ((1 - 0.6) * 1) + (0.6 * (temp.r / 255));
+							opaque.g = ((1 - 0.6) * 1) + (0.6 * (temp.g / 255))
+							opaque.b = ((1 - 0.6) * 1) + (0.6 * (temp.b / 255))
+							var col = "rgb(" +Math.floor(opaque.r * 255 ) +","
+								+ Math.floor(opaque.g * 255) +","+Math.floor(opaque.b * 255)+ ")"
+							//~ var trans = "rgba(" +temp.r+","+temp.g+","+temp.b+ ", 0.6)"
 							table += "<tr><td style='width:75px;margin:10px;background:"
-									+ trans + ";border:1px solid " 
+									+ col + ";border:1px solid " 
 									+ range[i%20] + ";'></td><td>"
 									+ domain[i] +"</td></tr>";
 							//~ console.log(i + " "+ domain[i] + " " + range[i]);
@@ -587,35 +566,42 @@
 				}
 			};
 			  			
-			//forced to use jquery dialog for floaty internal frame
-	/*		$("#spectrum").dialog({resizable:true, autoOpen: false, width: 600, height: 450});
-			//init spectrum viewer
-			var pepFragDiv = document.getElementById('pepFragDiv');
-			var graphDiv = document.getElementById('graphDiv');
-			spectrumViewer = new SpectrumViewer(pepFragDiv, graphDiv);
-			
-			function loadSpectra(id, pepSeq1, linkPos1, pepSeq2, linkPos2){
-				//jquery dialog open
-				$( "#spectrum" ).dialog("open");
-				
-				//~ var out = id +"|"+ pepSeq1 +"|"+ linkPos1 +"|"+ pepSeq2 +"|"+ linkPos2;
-				//~ var spectrumInnerDiv = document.getElementById("spectrum_inner_div");
-				//~ spectrumInnerDiv.innerHTML = out; 
-				spectrumViewer.clear();
-				var xmlhttp = new XMLHttpRequest();
-				var url = "./php/spectra.php";
-				var params =  "id=" + id;
-				xmlhttp.open("POST", url, true);
-				//Send the proper header information along with the request
-				xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xmlhttp.onreadystatechange = function() {//Call a function when the state changes.
-					if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-						spectrumViewer.setData(pepSeq1, linkPos1, pepSeq2, linkPos2, xmlhttp.responseText); 
-					}
-				}
-				xmlhttp.send(params);
-			}*/
+
 			//]]>
 		</script>
-	</body>
+		
+<!--
+		slider
+-->
+				<script type="text/javascript">	
+					//<![CDATA[					
+					var sliderDecimalPlaces = 1;
+					function getMinScore(){
+						if (xlv.scores){
+							var powerOfTen = Math.pow(10, sliderDecimalPlaces); 
+							return (Math.floor(xlv.scores.min * powerOfTen) / powerOfTen)
+									.toFixed(sliderDecimalPlaces);
+						}
+					}
+					function getMaxScore(){
+						if (xlv.scores){
+							var powerOfTen = Math.pow(10, sliderDecimalPlaces); 
+							return (Math.ceil(xlv.scores.max * powerOfTen) / powerOfTen)
+									.toFixed(sliderDecimalPlaces);
+						}
+					}
+					function sliderChanged(){
+						var slide = document.getElementById('slide');
+						var powerOfTen = Math.pow(10, sliderDecimalPlaces); 
+						
+						var cut = ((slide.value / 100) 
+									* (getMaxScore() - getMinScore()))
+									+ (getMinScore() / 1);
+						cut = cut.toFixed(sliderDecimalPlaces);
+						var cutoffLabel = document.getElementById("cutoffLabel");
+						cutoffLabel.innerHTML = '(' + cut + ')';
+						xlv.setCutOff(cut);
+					}						
+					//]]>
+				</script>
 </html>
