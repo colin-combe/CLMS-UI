fa4cbbbcc8b09e20735a34e4f5baab5c6f9b9f63
Colin Combe <colin.combe@ed.ac.uk>
2015-09-01 13:36:02 +0100

    clear spectrum (again) before setting data, addresses possible ajax synchronisation issue

--- php/networkScript.php
+++ php/networkScript.php
@@ -183,6 +183,7 @@
 		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		xmlhttp.onreadystatechange = function() {//Call a function when the state changes.
 			if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				spectrumViewer.clear();// tidy up, could be AJAX synchronisation issues
 				spectrumViewer.setData(pepSeq1, linkPos1, pepSeq2, linkPos2, xmlhttp.responseText);
 			}
 		}
