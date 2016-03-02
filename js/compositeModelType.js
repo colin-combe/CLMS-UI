b19df49165e092900ea2c3c792bf9dbbebffbecb
Colin Combe <colin.combe@ed.ac.uk>
2016-03-02 14:23:57 +0000

    fixed exports

--- js/compositeModelType.js
+++ js/compositeModelType.js
@@ -12,7 +12,7 @@
                 var unfilteredMatchCount = crossLink.matches.length;
                 for (var i = 0; i < unfilteredMatchCount; i++){
                     var match = crossLink.matches[i];
-                    var result = filterModel.filter(match);
+                    var result = filterModel.filter(match[0]); // terrible hack here, that match shouldn't be an array
                     //console.log("result:"+result);
                     if (result === true){
                         crossLink.filteredMatches.push(match);
