2f31e522075a30b2a81726d0a61f19ce180982f0
Martin Graham <martinjamesgraham@outlook.com>
2016-06-28 13:56:37 +0100

    semantic legend colour scale labels

--- js/linkColourAssignment.js
+++ js/linkColourAssignment.js
@@ -23,51 +23,54 @@
 		//check if link uniquely belongs to one group
 		var groupCheck = d3.set();
 		var filteredMatches = crossLink.filteredMatches;
-		for (match of filteredMatches) {
+		for (var match of filteredMatches) {
 			var match = match[0];//fix this weirdness with array ( [0] ), its so wrong
 			var group = searches.get(match.searchId).group; 	
 			groupCheck.add(group);
 		}
-		if (groupCheck.values().length == 1){
-			return CLMSUI.linkColour.groupColourScale(groupCheck.values()[0]);
-		}
-		else  {
-			return CLMSUI.linkColour.groupColourScale(-1);
-		}			
+        var scale = CLMSUI.linkColour.groupColourScale;
+        // choose value or right unknown value (linear scales don't do undefined)
+        var value = (groupCheck.values().length === 1 ? groupCheck.values()[0] : (scale.domain()[0] === -1 ? -1 : undefined));
+		return scale (value);		
 	};
     CLMSUI.linkColour.byGroup.init = function () {
         var searches = CLMSUI.compositeModelInst.get("clmsModel").get("searches");
         if (typeof CLMSUI.linkColour.groupColourScale == 'undefined') {
 			//put d3.scale for group colour assignment in compositeModel
-            var groups = new Set();
-			for(search of searches) {
-				groups.add(search.group);
+            var groups = new Map();
+			for(var search of searches) {
+                var val = search[1];
+                var arr = groups.get(val.group);
+                if (!arr) {
+                    arr = [];
+				    groups.set (val.group, arr);
+                }
+                arr.push (val.id);
 			}
+            
+            var groupDomain = [undefined];
+            var labelRange = ["Multiple Group"];
+            for (var group of groups) {
+                groupDomain.push (group[0]);
+                labelRange.push ("Group "+group[0]+" ("+group[1].join(", ")+")");
+            }
 			var groupCount = groups.size; 
-            var labels = [];
+
 			if (groupCount < 6) {
-                var colArr = colorbrewer.Dark2[5];
-                colArr = ["black"].concat(colArr);
-				CLMSUI.linkColour.groupColourScale = d3.scale.ordinal().range(colArr).domain(d3.range(-1, 5, 1));
-                labels = CLMSUI.linkColour.groupColourScale.domain().map (function (d) {
-                    return (d < 0 ? "Multiple" : "Group "+d);
-                });
+                var colArr = ["grey"].concat(colorbrewer.Dark2[5]);
+				CLMSUI.linkColour.groupColourScale = d3.scale.ordinal().range(colArr).domain(groupDomain);
 			}
 			else if (groupCount < 11){
-                var colArr = colorbrewer.Paired[10];
-                colArr = ["black"].concat(colArr);
-				CLMSUI.linkColour.groupColourScale = d3.scale.ordinal().range(colArr).domain(d3.range(-1, 10, 1));
-                labels = CLMSUI.linkColour.groupColourScale.domain().map (function (d) {
-                    return (d < 0 ? "Multiple" : "Group "+d);
-                });
+                var colArr = ["grey"].concat(colorbrewer.Paired[10]);
+				CLMSUI.linkColour.groupColourScale = d3.scale.ordinal().range(colArr).domain(groupDomain);
 			}	
 			else { // more than 10 groups, not really feasible to find colour scale that works
 				//a d3.scale that always returns gray?
-                CLMSUI.linkColour.groupColourScale = d3.scale.linear().domain([-1,0]).range(["black", "#767676"]).clamp(true);
-                labels = ["Cross-Group", "Single"];
+                CLMSUI.linkColour.groupColourScale = d3.scale.linear().domain([-1,0]).range(["grey", "#448866"]).clamp(true);
+                labelRange = ["Multiple Group", "Single Group"];
 			}
             CLMSUI.linkColour.byGroup.colScale = CLMSUI.linkColour.groupColourScale;
-            CLMSUI.linkColour.byGroup.labels = CLMSUI.linkColour.byGroup.colScale.copy().range(labels);
+            CLMSUI.linkColour.byGroup.labels = CLMSUI.linkColour.byGroup.colScale.copy().range(labelRange);
             console.log ("colscale", CLMSUI.linkColour.groupColourScale);
 		}
     };
