20c669b5bdd1031229063b1b5bb705b9355ac4e3
Colin Combe <colin.combe@ed.ac.uk>
2017-01-06 09:37:12 +0000

    annotation menu ordering fix, xinet axis labels fix

--- js/annotationTypeModel.js
+++ js/annotationTypeModel.js
@@ -11,12 +11,16 @@ CLMSUI.BackboneModelTypes.AnnotationType = Backbone.Model.extend ({
     initialize: function (options) {
             var defaultOptions = {};
 			this.options = _.extend(defaultOptions, options);
-			this.set("category", options.category + "-" +  options.type);
+			this.set("id", options.category + "-" +  options.type);
 			this.set("category", options.category);
  			this.set("type", options.type);
     },
+
 });
 
 CLMSUI.BackboneModelTypes.AnnotationTypeCollection = Backbone.Collection.extend ({
     model: CLMSUI.BackboneModelTypes.AnnotationType,
+    comparator: function (model) {
+        return model.get("id");
+    },
 });
