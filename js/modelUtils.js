4fb990239f526b3fa91179a5cc864972b21b60e5
Martin Graham <martinjamesgraham@outlook.com>
2018-03-05 11:56:20 +0000

    xmlversion field removed in firefox, use contentType instead

--- js/modelUtils.js
+++ js/modelUtils.js
@@ -393,7 +393,8 @@ CLMSUI.modelUtils = {
         $.get("https://www.rcsb.org/pdb/rest/das/pdb_uniprot_mapping/alignment?query="+pdbCode,
             function (data, status, xhr) {   
                 //console.log ("data", data, arguments);
-                if (status === "success" && data.xmlVersion) {  // data is an xml fragment
+			
+                if (status === "success" && data.contentType === "text/xml") {  // data is an xml fragment
                     var map = d3.map();
 
                     $(data).find("block").each (function(i,b) { 
