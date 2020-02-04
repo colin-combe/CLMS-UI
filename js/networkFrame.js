faa419ca7eb645c3b9edbaae2a4c45e272c30e31
colin-combe <colin.combe@ed.ac.uk>
2020-02-04 13:51:23 +0000

    comment out crosslink CSV upload

--- js/networkFrame.js
+++ js/networkFrame.js
@@ -562,11 +562,11 @@ CLMSUI.init.views = function() {
             eventName: "stringDataChooserShow",
             tooltip: "Load STRING data from the STRING server. Note: limited to <2,000 proteins, for more generate a CSV file for import as PPI Metadata"
         },
-        {
-            name: "Crosslinks (CSV)",
-            eventName: "csvFileChooserShow",
-            tooltip: "Load Cross-Links from a local CSV File"
-        },
+        // {
+        //     name: "Crosslinks (CSV)",
+        //     eventName: "csvFileChooserShow",
+        //     tooltip: "Load Cross-Links from a local CSV File"
+        // },
         {
             name: "Crosslink or PPI Metadata",
             eventName: "linkMetaDataFileChooserShow",
